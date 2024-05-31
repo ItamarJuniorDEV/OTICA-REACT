03aba0e165b047b767cd17139268920c12792126
Itamar Junior <cdajuniorf@gmail.com>
2024-05-31 18:48:04 -0300

    Primeiro commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../css/Footer.module.css';
+
+
+export const Footer = () => {
+  return (
+    <section className={styles.footer}>
+      <div>
+        <p>©️2022 Óticas Vida - Todos os direitos reservados | Itamar Junior.</p>
+      </div>
+    </section>
+  );
+};
+
+
